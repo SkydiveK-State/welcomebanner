394bec2d21d9bb4439b1d9c1d4939d3e9d198353
Kevin Dice <kevin.dice1@gmail.com>
2016-07-20 12:28:44 -0400

    Rebrand

--- system_banner.sh
+++ system_banner.sh
@@ -1,10 +1,11 @@
 PURPLE='\033[0;35m'
 NC='\033[0m' # No Color
 
-printf "${PURPLE}" > /tmp/motd
-echo "Hack\nK-State" | toilet --font NScript >> /tmp/motd
+echo -e "\e[38;5;93m" > /tmp/motd
+echo "Skydive
+K-State" | toilet --font NScript >> /tmp/motd
 printf "${NC}" >> /tmp/motd
 linuxlogo -gu >> /tmp/motd
 
 # Remove whitespace
-cat /tmp/motd | sed -e '12,16d;27,31d' > /etc/motd
+cat /tmp/motd | sed -e '28,32d' > /etc/motd
